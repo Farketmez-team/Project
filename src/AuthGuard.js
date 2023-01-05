7423a3edf81298abfc60f777da8f1a15066f1123
hatem cinipi <hatememre.cinipi2016@gtu.edu.tr>
2023-01-05 16:47:18 +0300

    botox and lifting table

--- src/AuthGuard.js
+++ src/AuthGuard.js
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const useAuth = ()=>{
     const user = JSON.parse(localStorage.getItem('user'))
     //console.log(user)
-    return user;
+    return true;
 }
 
 function AuthGuard() {
